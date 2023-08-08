e052106b4cb80b2a4776e5b5fe5169d59db1097b
Anatoly Politsin <politsin@gmail.com>
2023-08-08 22:37:23 +0300

    8.0

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /opt/build/docker-mattermost
 docker build -t synstd/mattermost .
-docker tag synstd/mattermost synstd/mattermost:7.9.1
+docker tag synstd/mattermost synstd/mattermost:8.0.1
 docker login --username=synstd
 # тут нужно ввести пароль
 
 docker push synstd/mattermost
-docker push synstd/mattermost:7.9.1
+docker push synstd/mattermost:8.0.1
