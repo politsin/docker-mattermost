cdec4a1ff147f65a38352a557cf641b7b764a14c
Anatoly Politsin <politsin@gmail.com>
2023-09-01 15:38:42 +0300

    fix

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /opt/build/docker-mattermost
 docker build -t synstd/mattermost .
-docker tag synstd/mattermost synstd/mattermost:8.0.1
+docker tag synstd/mattermost synstd/mattermost:8.1.0
 docker login --username=synstd
 # тут нужно ввести пароль
 
